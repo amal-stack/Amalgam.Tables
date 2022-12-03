0a0e7fd22a0dc013351b5536798b611ce4c3e6cc
amalkrishna <amalkrishna263@outlook.com>
2022-12-03 19:46:07 +0530

    Update enum constants

--- src/PlainText/TableRule.cs
+++ src/PlainText/TableRule.cs
@@ -1,4 +1,4 @@
-﻿namespace Amalgam.Tables.PlainText;
+﻿namespace Amalgam.Tables.Plaintext;
 
 /// <summary>
 /// Defines flags that specify the areas within a plain text table where a horizontal rule is desired.
@@ -8,8 +8,8 @@ public enum TableRule
 {
     None = 0,
     HeaderTop = 1,
-    HeaderBottom = 1 << 2,
-    Body = 1 << 4,
+    HeaderBottom = 1 << 1,
+    Body = 1 << 2,
     Footer = 1 << 3,
     Header = HeaderTop | HeaderBottom,
     HeaderFooter = Header | Footer,
